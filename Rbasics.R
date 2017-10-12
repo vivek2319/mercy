d83ca427af0862158330479db02d9efb2a7fd146
Vivek Kulkarni <vivek2319@gmail.com>
2017-10-12 17:18:46 -0400

    Basic R operations

---
+++ Rbasics.R
@@ -0,0 +1,53 @@
+#Some of the R basics 
+
+#Addition
+2+2
+#Output:- [1] 4
+
+-2+(-7)
+#Output:- [1] -9
+
+0+0
+#Output:- [1] 0
+
+#Subtraction
+4-2
+#Output:- [1] 2
+
+2-6
+#Output:- [1] -4
+
+-7-8
+#Output:- [1] -15
+
+#Multiplication
+2*2
+#Output:- [1] 4
+
+0.3*4.6
+#Output:- [1] 1.38
+
+-6*-8
+#Output:- [1] 48
+
+-9*0.5
+#Output:- [1] -4.5
+
+#Division
+10/2
+#Output:- [1] 5
+
+10/0.3
+#Output:- [1] 33.33333
+
+0.0555/12
+#Output:- [1] 0.004625
+
+#Exponentioan
+2^6
+#Output:- [1] 64
+
+2**6
+#Output:- [1] 64
+
+
