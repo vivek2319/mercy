89098abe2c13c9cd98e555ba3bebf56e896bd669
Vivek Kulkarni <vivek2319@gmail.com>
2017-10-17 00:49:26 -0400

    numpy with basic plots

---
+++ numpywithplots.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+a = np.array([1234,2344, 6786, 5467, 4467, 7896, 4567, 9876, 1433])
+print(data[:10])
+b = np.array([121,234, 678, 546, 446, 789, 456, 987, 143])
+plt.scatter(a,b,s=10)
+plt.title("Practicing some things in Python")
+plt.xlabel("salary in thousand $")
+plt.ylabel(" per hour rate")
+plt.grid(True, linestyle='-', color='0.75')
+plt.show()
