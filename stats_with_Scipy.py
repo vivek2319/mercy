85b834833e76eeb3bb84ff3b1c3c617dbc8d4200
Vivek Kulkarni <vivek2319@gmail.com>
2018-01-17 14:07:44 -0500

    Stats using Scipy

---
+++ stats_with_Scipy.py
@@ -0,0 +1,17 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import numpy as np
+>>> a = np.random.normal(0,1,size=100)
+>>> b = np.random.normal(1,1,size=100)
+>>> stats.ttest_ind(a,b)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    stats.ttest_ind(a,b)
+NameError: name 'stats' is not defined
+>>> from scipy import scipy.stats
+SyntaxError: invalid syntax
+>>> from scipy import stats
+>>> stats.ttest_ind(a,b)
+
+Ttest_indResult(statistic=-9.7738812433919353, pvalue=1.1610896913232291e-18)
+>>> 
