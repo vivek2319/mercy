ea36013f7ca6d0153a78181a338fa85c42f8e510
Vivek Kulkarni <vivek2319@gmail.com>
2018-01-17 14:09:31 -0500

    Update stats_with_Scipy.py

--- stats_with_Scipy.py
+++ stats_with_Scipy.py
@@ -1,5 +1,5 @@
-Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
+#Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+#Type "copyright", "credits" or "license()" for more information.
 >>> import numpy as np
 >>> a = np.random.normal(0,1,size=100)
 >>> b = np.random.normal(1,1,size=100)
@@ -15,3 +15,11 @@ SyntaxError: invalid syntax
 
 Ttest_indResult(statistic=-9.7738812433919353, pvalue=1.1610896913232291e-18)
 >>> 
+#The resulting output is composed of:
+
+#The T statistic value: it is a number the sign of which is proportional to the difference between the two random processes and 
+#the magnitude is related to the significance of this difference.
+    
+  
+#The p value: the probability of both processes being identical. If it is close to 1, the two process are almost certainly 
+#identical. The closer it is to zero, the more likely it is that the processes have different means.
