bf1678ced13425149ee00b2db9791595447b031e
Vivek Kulkarni <vivek2319@gmail.com>
2019-05-01 10:10:24 -0700

    Update drop-na-values.py

--- drop-na-values.py
+++ drop-na-values.py
@@ -2,3 +2,9 @@ Best way to remove na values; do it inplace without reassingning it
 
 df_all.dropna(inplace=True)
 df_all.reset_index(drop=True, inplace=True)
+
+
+OR
+
+df_all = df_all.dropna()
+df_all = df_all.reset_index(drop=True)
